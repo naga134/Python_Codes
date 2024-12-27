ee11035835c5dedd66cd062f635633aeb224e74a
Pravallika <happylife200414@gmail.com>
2024-12-27 16:12:05 +0100

    CAPTCHA

--- captcha.py
+++ captcha.py
@@ -3,8 +3,17 @@ from PIL import Image
 import random
 import string
 
-def generate_captcha_text(lenght=10):
-    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=lenght))
+def generate_captcha_text(length=10):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
 def generate_captcha_image(captcha_text, image_width=300):
     image = ImageCaptcha(image_width)
+    image_file = f"{captcha_text}.png"
+    image.write(captcha_text, image_file)
+    return image_file
+
+captcha_text = generate_captcha_text()
+image_file = generate_captcha_image(captcha_text)
+
+print(f"Generated CAPTCHA: {captcha_text}")
+Image.open(image_file).show()
